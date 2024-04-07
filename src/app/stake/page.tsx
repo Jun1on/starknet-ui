6e714fed096f519b02318d2d557e0354b0a279b6
Junion <69495294+Jun1on@users.noreply.github.com>
2024-04-07 06:11:29 -0500

    visual tweaks

--- src/app/stake/page.tsx
+++ src/app/stake/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center flex flex-col gap-8">
               <header>
-                  <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+                  <h1 className="text-3xl font-bold leading-tight tracking-tight">
                       STAKE
                   </h1>
               </header>
