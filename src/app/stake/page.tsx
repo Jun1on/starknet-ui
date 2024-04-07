893d7f8af7e04702f2d81329e2ab8a2ff214c76d
Junion <69495294+Jun1on@users.noreply.github.com>
2024-04-07 03:13:17 -0500

    clean up project

--- src/app/stake/page.tsx
+++ src/app/stake/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center flex flex-col gap-8">
               <header>
                   <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
                       STAKE
