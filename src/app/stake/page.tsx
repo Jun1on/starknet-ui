4b3854308ff4ff4ccfaa6b3132270f8edc165f15
Junion <69495294+Jun1on@users.noreply.github.com>
2024-04-06 20:33:48 -0500

    fix connect-modal bug

---
+++ src/app/stake/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center">
+              <header>
+                  <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+                      STAKE
+                  </h1>
+              </header>
+              <p>
+                  Get started with your dApp development with pre-configed setting for wallet connection and Starknet React library for interacting with Starknet blockchain.
+              </p>
+          </div>
+      );
+  }
+  
