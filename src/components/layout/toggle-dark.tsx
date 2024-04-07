577ea7f085a21313b591f2eec3866c61d4b97b1b
Junion <jczhang001@gmail.com>
2024-04-06 23:55:38 -0500

    landing page

---
+++ src/components/layout/toggle-dark.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+
+export default function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  // Function to toggle between 'light' and 'dark' theme
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      {theme === "dark" ? (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      )}
+    </Button>
+  );
+}
